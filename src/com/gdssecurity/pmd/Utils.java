fc4ce03f776979ed96a1bbcad28bc7273161318a
alberto <infjaf@gmail.com>
2015-06-21 21:07:14 +0200

    Treats class fields as tainted sources.

--- src/com/gdssecurity/pmd/Utils.java
+++ src/com/gdssecurity/pmd/Utils.java
@@ -16,6 +16,7 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.HashSet;
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -23,8 +24,14 @@ import net.sourceforge.pmd.RuleContext;
 import net.sourceforge.pmd.lang.ast.Node;
 import net.sourceforge.pmd.lang.java.ast.ASTClassOrInterfaceType;
 import net.sourceforge.pmd.lang.java.ast.ASTExpression;
+import net.sourceforge.pmd.lang.java.ast.ASTFieldDeclaration;
 import net.sourceforge.pmd.lang.java.ast.ASTName;
 import net.sourceforge.pmd.lang.java.ast.ASTPrimaryExpression;
+import net.sourceforge.pmd.lang.java.ast.ASTPrimaryPrefix;
+import net.sourceforge.pmd.lang.java.ast.ASTPrimarySuffix;
+import net.sourceforge.pmd.lang.java.ast.ASTTypeDeclaration;
+import net.sourceforge.pmd.lang.java.ast.ASTVariableDeclarator;
+import net.sourceforge.pmd.lang.java.ast.ASTVariableDeclaratorId;
 
 
 public final class Utils {
@@ -76,8 +83,17 @@ public final class Utils {
             } else if (node instanceof ASTPrimaryExpression) {
                 if (node.hasDescendantOfType(ASTClassOrInterfaceType.class)) {					
                     type = node.getFirstDescendantOfType(ASTClassOrInterfaceType.class).getType();
-                } else {					
-                    type = node.getFirstDescendantOfType(ASTName.class).getType();
+                } else {	
+                	ASTPrimaryPrefix prefix = node.getFirstChildOfType(ASTPrimaryPrefix.class);
+                	ASTName astName = prefix.getFirstChildOfType(ASTName.class);        	
+                	if (astName != null) {
+                		type = node.getFirstDescendantOfType(ASTName.class).getType();
+                	}
+                	else {
+                		ASTPrimarySuffix suffix = node.getFirstChildOfType(ASTPrimarySuffix.class);
+                		type = getFieldType(node, suffix.getImage());
+                	}
+                    
                 }
             } else if (node instanceof ASTName) {
                 type = ((ASTName) node).getType();
@@ -100,7 +116,27 @@ public final class Utils {
         return cannonicalName;
     }
 	
-    public static HashSet<String> arrayAsHashSet(String[] array) {
+    private static Class<? extends Object> getFieldType(Node node, String fieldName) {
+		ASTTypeDeclaration classDeclaration = node.getFirstParentOfType(ASTTypeDeclaration.class);
+		if (classDeclaration == null) {
+			return null;
+		}
+		List<ASTFieldDeclaration> fields = classDeclaration.findDescendantsOfType(ASTFieldDeclaration.class);
+		for (ASTFieldDeclaration field : fields) {
+			ASTVariableDeclarator declarator = field.getFirstChildOfType(ASTVariableDeclarator.class);
+			ASTVariableDeclaratorId name1 = declarator.getFirstChildOfType(ASTVariableDeclaratorId.class);
+			if (name1 != null) {
+				String name = name1.getImage();
+				if (name.equals(fieldName)) {
+					return name1.getType();
+				}
+			}
+			
+		}    	
+		return null;
+	}
+
+	public static HashSet<String> arrayAsHashSet(String[] array) {
         HashSet<String> hashSet = new HashSet<String>(array.length);
         int nbItem = 0;
 
